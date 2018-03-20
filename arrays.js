0ab472b065d804529cdb7b8c3fa93c0ac406a4cb
Christopher <christopherccm@msn.com>
2018-03-20 23:30:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,23 +1,18 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-//var rockTypes = ['igneous', 'basalt', 'granite'];
 
 function addElementToBeginningOfArray(array, element) {
   const newArray = [element, ...array];
   return newArray;
 }
 
-//addElementToBeginningOfArray(rockTypes, 'sedimentary'};
-
-//var rockTypes = ['igneous', 'basalt', 'granite'];
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
   return array;
 }
 
-function accessElementInArray(array, indexNo) {
-  return array[indexNo];
-}
 
-//destructivelyAddElementToBeginningOfArray(rockTypes, 'sedimentary'}
+function accessElementInArray(array, indexNumber) {
+  return array[indexNumber];
+}
