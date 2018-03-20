3995d199d2c94bb91558f1443ee7b1dbd25b723d
Christopher <christopherccm@msn.com>
2018-03-20 23:57:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -46,3 +46,9 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop();
   return array;
 }
+
+
+function removeElementFromEndOfArray(array) {
+  const newArray = array.slice(0, array.length - 1);
+  return newArray;
+}
